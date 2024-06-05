36708f2de132b2d70bcece4573ce149e6479349e
Hwang-Jaeryeong <jryeong67@naver.com>
2024-06-05 22:36:07 +0900

    [fix] res, wc block: 병렬처리

--- model/BUnet.py
+++ model/BUnet.py
@@ -20,47 +20,135 @@ class ConvBlock(nn.Module):
         return x
 
 class RESBlock(nn.Module):
+
     def __init__(self, in_channels, out_channels):
         super(RESBlock, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=1)
-        self.bn1 = nn.BatchNorm2d(out_channels)
-        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
-        self.bn2 = nn.BatchNorm2d(out_channels)
-        self.conv3 = nn.Conv2d(out_channels, out_channels, kernel_size=1)
-        self.bn3 = nn.BatchNorm2d(out_channels)
-        self.conv_residual = nn.Conv2d(in_channels, out_channels, kernel_size=1)
-        self.bn_residual = nn.BatchNorm2d(out_channels)
 
+        self.split_conv_x1_1 = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=(15, 1)),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+            )
+        self.split_conv_x1_2 = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=(1, 15)),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+            )
+        
+        self.split_conv_x2_1 = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=(13, 1)),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+            )
+        self.split_conv_x2_2 = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=(1, 13)),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+            )
+        
+        self.split_conv_x3_1 = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=(11, 1)),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+            )
+        self.split_conv_x3_2 = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=(1, 11)),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+            )
+        
+        self.split_conv_x4_1 = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=(9, 1)),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+            )
+        self.split_conv_x4_2 = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=(1, 9)),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+            )
+        
+        self.sum_conv_x1 = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+            )
+        self.sum_conv_x2 = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+            )
+        self.sum_conv_x3 = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+            )
+        
     def forward(self, x):
-        residual = self.conv_residual(x)
-        residual = self.bn_residual(residual)
+        init = x
 
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = F.relu(x)
-
-        x = self.conv2(x)
-        x = self.bn2(x)
-        x = F.relu(x)
-
-        x = self.conv3(x)
-        x = self.bn3(x)
-
-        x += residual
-        x = F.relu(x)
+        split_conv_x1 = self.split_conv_x1_1(x)
+        split_conv_x1 = self.split_conv_x1_2(split_conv_x1)
+        
+        split_conv_x2 = self.split_conv_x2_1(x)
+        split_conv_x2 = self.split_conv_x2_2(split_conv_x2)
+        
+        split_conv_x3 = self.split_conv_x3_1(x)
+        split_conv_x3 = self.split_conv_x3_2(split_conv_x3)
+        
+        split_conv_x4 = self.split_conv_x4_1(x)
+        split_conv_x4 = self.split_conv_x4_2(split_conv_x4)
+        
+        x = torch.cat([init, split_conv_x1, split_conv_x2, split_conv_x3, split_conv_x4], dim=1)
+        
+        x = self.sum_conv_x1(x)
+        x = self.sum_conv_x2(x)
+        x = self.sum_conv_x3(x)
 
         return x
+        
 
 class WCBlock(nn.Module):
+
     def __init__(self, in_channels, out_channels):
         super(WCBlock, self).__init__()
-        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
-        self.bn = nn.BatchNorm2d(out_channels)
 
+        self.split_conv_x1_1 = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=(15, 1)),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+            )
+        self.split_conv_x1_2 = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=(1, 15)),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+            )
+        
+        self.split_conv_x2_1 = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=(1, 15)),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+            )
+        self.split_conv_x2_2 = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=(15, 1)),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+            )
+        
+        self.batch_norm = nn.BatchNorm2d(out_channels)
+        
     def forward(self, x):
-        x = self.conv(x)
-        x = self.bn(x)
-        x = F.relu(x)
+
+        split_conv_x1 = self.split_conv_x1_1(x)
+        split_conv_x1 = self.split_conv_x1_2(split_conv_x1)
+        
+        split_conv_x2 = self.split_conv_x2_1(x)
+        split_conv_x2 = self.split_conv_x2_2(split_conv_x2)
+        
+        x = torch.cat([split_conv_x1, split_conv_x2], dim=1)
+        x = self.batch_norm(x)
+        x = nn.ReLu(x)
+        
         return x
 
 class EncoderBlock(nn.Module):
@@ -124,6 +212,7 @@ class BUNet(nn.Module):
         d4 = self.decoder4(d3, s1)
 
         outputs = self.out_conv(d4)
+        outputs = torch.sigmoid(outputs)
         return outputs
 
 # 모델 초기화
