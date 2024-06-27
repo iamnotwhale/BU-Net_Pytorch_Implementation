65628566c085c6adc5c196cefbf2090d6feb8b37
권보영 <kby2009@korea.ac.kr>
2024-06-27 15:51:06 +0900

    [add] py files

--- utils/utils.py
+++ utils/utils.py
@@ -9,4 +9,13 @@ def visualize_loss(history):
     plt.xlabel('Epoch')
     plt.ylabel('Loss')
     plt.legend()
-    plt.show()
+    plt.savefig('loss_plot.png')
+
+def visualize_accuracy(history):
+    plt.figure(figsize=(10, 5))
+    plt.plot(history['train_acc'], label='train accuracy')
+    plt.plot(history['val_acc'], label='val accuracy')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.legend()
+    plt.savefig('accuracy_plot.png')
